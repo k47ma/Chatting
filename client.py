0a3e0fce10b07be149bb64cc7ebcb43c1e92d1b1
Kai Ma <k47ma@edu.uwaterloo.ca>
2017-08-14 12:35:49 -0400

    Initial commit

---
+++ client.py
@@ -0,0 +1,40 @@
+import socket
+import threading
+
+
+class Client(object):
+    def __init__(self):
+        object.__init__(self)
+        self.s = socket.socket()
+        host = "TOR05INTERND1"
+        port = 12345
+        self.s.connect((host, port))
+        print "connected to:", socket.getaddrinfo(host, port)[1][-1]
+        ReceiveMessageThread(self.s).start()
+        SendMessageThread(self.s).start()
+
+
+class ReceiveMessageThread(threading.Thread):
+    def __init__(self, connection):
+        threading.Thread.__init__(self)
+        self.connection = connection
+
+    def run(self):
+        while True:
+            message = self.connection.recv(1024)
+            print "Server: " + message
+
+
+class SendMessageThread(threading.Thread):
+    def __init__(self, connection):
+        threading.Thread.__init__(self)
+        self.connection = connection
+
+    def run(self):
+        message = raw_input()
+        self.connection.send(message)
+        print "Client: " + message
+
+
+if __name__ == '__main__':
+    Client()
---
+++ server.py
@@ -0,0 +1,54 @@
+import socket
+import threading
+
+
+class Server(object):
+    def __init__(self):
+        object.__init__(self)
+        self.s = socket.socket()
+        host = socket.gethostname()
+        port = 12345
+        self.s.bind((host, port))
+        self.s.listen(5)
+        self.wait_for_client()
+
+    def wait_for_client(self):
+        while True:
+            print "waiting for connection..."
+            client, addr = self.s.accept()
+            print "connected from:", addr
+            ReceiveMessageThread(client).start()
+            SendMessageThread(client).start()
+
+
+class ReceiveMessageThread(threading.Thread):
+    def __init__(self, client):
+        threading.Thread.__init__(self)
+        self.client = client
+
+    def run(self):
+        while True:
+            try:
+                message = self.client.recv(1024)
+                print "Client: " + message
+            except Exception:
+                break
+
+
+class SendMessageThread(threading.Thread):
+    def __init__(self, client):
+        threading.Thread.__init__(self)
+        self.client = client
+
+    def run(self):
+        while True:
+            try:
+                message = raw_input()
+                self.client.send(message)
+                print "Server: " + message
+            except Exception:
+                break
+
+
+if __name__ == '__main__':
+    Server()
