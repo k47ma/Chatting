18969a0d81a793022ac44bf60ee9fb15f187938e
Kai Ma <k47ma@edu.uwaterloo.ca>
2017-08-14 14:53:56 -0400

    - Added interface

--- client.py
+++ client.py
@@ -1,36 +1,96 @@
 import socket
 import threading
+from tkinter import *
 
 
-class Client(object):
-    def __init__(self):
-        object.__init__(self)
-        self.s = socket.socket()
+class ClientInterface(Tk):
+    def __init__(self, *args, **kwargs):
+        Tk.__init__(self, *args, **kwargs)
+
+        self.wm_title("Chatting")
+        self.wm_geometry("500x300")
+
+        container = Frame(self)
+        container.pack(fill=BOTH, expand=True)
+
+        self.entry = Entry(container, state=DISABLED)
+        self.entry.pack(side=BOTTOM, padx=6, pady=(0, 6), fill=X)
+
+        self.textarea = Text(container)
+        self.textarea.pack(side=TOP, padx=6, pady=6, fill=BOTH, expand=True)
+
+        self.setup_server()
+
+    def setup_server(self):
+        client = Client(self)
+        client.daemon = True
+        client.start()
+
+
+class Client(threading.Thread):
+    def __init__(self, controller):
+        threading.Thread.__init__(self)
+
+        self.entry = controller.entry
+        self.textarea = controller.textarea
+
+    def run(self):
+        s = socket.socket()
         host = socket.gethostname()
         port = 12345
-        self.s.connect((host, port))
-        print "connected to:", socket.getaddrinfo(host, port)[1][-1]
-        ReceiveMessageThread(self.s).start()
-        SendMessageThread(self.s).start()
+        try:
+            s.connect((host, port))
+        except Exception:
+            show_message("Can't connect to the server!", self.textarea)
+            return
+
+        self.entry["state"] = NORMAL
+        self.entry.bind("<Return>", lambda x: self.send_message(s))
+
+        show_message("connected to: " + str(socket.getaddrinfo(host, port)[1][-1]), self.textarea)
+        ReceiveMessageThread(s, self.textarea).start()
+
+    def send_message(self, connection):
+        message = self.entry.get()
+        if message:
+            self.entry.delete(0, END)
+            connection.send(message)
+            show_message("Client: " + message, self.textarea)
 
 
 class ReceiveMessageThread(threading.Thread):
-    def __init__(self, connection):
+    def __init__(self, connection, textarea):
         threading.Thread.__init__(self)
+
         self.connection = connection
+        self.textarea = textarea
 
     def run(self):
         while True:
-            message = self.connection.recv(1024)
-            print "Server: " + message
+            try:
+                message = self.connection.recv(1024)
+                show_message("Server: " + message, self.textarea)
+            except Exception:
+                break
 
 
-class SendMessageThread(threading.Thread):
-    def __init__(self, connection):
+class ShowMessage(threading.Thread):
+    def __init__(self, message, textarea):
         threading.Thread.__init__(self)
-        self.connection = connection
+        self.message = message
+        self.textarea = textarea
 
     def run(self):
-        message = raw_input()
-        self.connection.send(message)
-        print "Client: " + message
+        self.textarea.insert(END, self.message)
+        self.textarea.see(END)
+
+
+def show_message(message, textarea):
+    thread = ShowMessage(message + "\n", textarea)
+    thread.daemon = True
+    thread.start()
+
+
+if __name__ == '__main__':
+    client = ClientInterface()
+    client.mainloop()
--- server.py
+++ server.py
@@ -1,50 +1,95 @@
 import socket
 import threading
+from tkinter import *
 
 
-class Server(object):
-    def __init__(self):
-        object.__init__(self)
+class ServerInterface(Tk):
+    def __init__(self, *args, **kwargs):
+        Tk.__init__(self, *args, **kwargs)
+
+        self.wm_title("Chatting")
+        self.wm_geometry("500x300")
+
+        container = Frame(self)
+        container.pack(fill=BOTH, expand=True)
+
+        self.entry = Entry(container, state=DISABLED)
+        self.entry.pack(side=BOTTOM, padx=6, pady=(0, 6), fill=X)
+
+        self.textarea = Text(container)
+        self.textarea.pack(side=TOP, padx=6, pady=6, fill=BOTH, expand=True)
+        self.textarea.insert(END, "Waiting for connection...\n")
+
+        self.setup_server()
+
+    def setup_server(self):
+        server = Server(self)
+        server.daemon = True
+        server.start()
+
+
+class Server(threading.Thread):
+    def __init__(self, controller):
+        threading.Thread.__init__(self)
+
+        self.entry = controller.entry
+        self.textarea = controller.textarea
+
         self.s = socket.socket()
         host = socket.gethostname()
         port = 12345
         self.s.bind((host, port))
         self.s.listen(5)
-        self.wait_for_client()
 
-    def wait_for_client(self):
+    def run(self):
         while True:
-            print "waiting for connection..."
             client, addr = self.s.accept()
-            print "connected from:", addr
-            ReceiveMessageThread(client).start()
-            SendMessageThread(client).start()
+            self.entry.bind("<Return>", lambda x: self.send_message(client))
+            show_message("connected from: " + str(addr), self.textarea)
+            self.entry["state"] = NORMAL
+            ReceiveMessageThread(client, self.textarea).start()
+
+    def send_message(self, client):
+        message = self.entry.get()
+        if message:
+            self.entry.delete(0, END)
+            client.send(message)
+            show_message("Server: " + message, self.textarea)
 
 
 class ReceiveMessageThread(threading.Thread):
-    def __init__(self, client):
+    def __init__(self, client, textarea):
         threading.Thread.__init__(self)
+
         self.client = client
+        self.textarea = textarea
 
     def run(self):
         while True:
             try:
                 message = self.client.recv(1024)
-                print "Client: " + message
+                show_message("Client: " + message, self.textarea)
             except Exception:
                 break
 
 
-class SendMessageThread(threading.Thread):
-    def __init__(self, client):
+class ShowMessage(threading.Thread):
+    def __init__(self, message, textarea):
         threading.Thread.__init__(self)
-        self.client = client
+        self.message = message
+        self.textarea = textarea
 
     def run(self):
-        while True:
-            try:
-                message = raw_input()
-                self.client.send(message)
-                print "Server: " + message
-            except Exception:
-                break
+        self.textarea.insert(END, self.message)
+        self.textarea.see(END)
+
+
+def show_message(message, textarea):
+    thread = ShowMessage(message + "\n", textarea)
+    thread.daemon = True
+    thread.start()
+
+
+if __name__ == '__main__':
+    chatting = ServerInterface()
+    chatting.mainloop()
